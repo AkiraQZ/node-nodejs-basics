df15d3029089aad1fc223ecc3d4ebf6092f69be2
AkiraQZ <vboyrkin@gmail.com>
2023-06-20 03:56:17 +0800

    feat:create.js complete

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
-const create = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises';
+
+  const name = 'fresh.txt'
+  const content = 'I am fresh and young'
+  const path = 'files/'
+
+
+async function create() {
+    const err = new Error('FS operation failed');
+    try {
+        await fs.open(path + name);
+
+        throw err;
+    } catch (e) {
+        if (e.code) {
+            fs.writeFile(path + name, content);
+            console.log('Success!');
+        } else {
+            throw err;
+        }
+    }
+    
+}
 
 await create();

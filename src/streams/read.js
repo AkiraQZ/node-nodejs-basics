1e2f86bea81d5b390d24bbde73e604feff8983e5
AkiraQZ <vboyrkin@gmail.com>
2023-06-20 04:13:00 +0800

    feat: stram complete

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
-const read = async () => {
-    // Write your code here 
+import * as fs from 'node:fs';
+import * as process from 'node:process';
+
+
+const file = 'files/fileToRead.txt'
+
+async function read(){
+    const str = fs.createReadStream(file)
+    
+    str.on('data', (buffer) => process.stdout.write(buffer))
+    str.on('end', () => console.log('\nSuccess!'))
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
-const transform = async () => {
-    // Write your code here 
+
+
+import * as process from 'node:process';
+import * as stream from 'node:stream';
+
+async function transform() {
+    const transform = new stream.Transform({
+        transform(chunk, enc, cb){
+            chunk = chunk.toString().split('').reverse().join('')
+            cb(null, chunk)
+        }
+    })
+
+process.stdin.pipe(transform).pipe(process.stdout)
+
 };
+
 
 await transform();

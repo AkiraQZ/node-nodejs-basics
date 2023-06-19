9aa79c484bbcf18d88bbf8c90afe80bf4dae69d1
AkiraQZ <vboyrkin@gmail.com>
2023-06-20 03:59:40 +0800

    feat:delete.js complete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
-const remove = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises';
+
+const name = 'fileToRemove.txt'
+const path = 'files/'
+
+const err = new Error('FS opperation failed')
+
+async function remove() {
+  try {
+    await fs.access(path+name, fs.constants.R_OK | fs.constants.W_OK);
+    fs.unlink(path+name)
+    console.log('Succesfuly deleted');
+  } catch(e) {
+    throw err
+  } 
+}
 
 await remove();

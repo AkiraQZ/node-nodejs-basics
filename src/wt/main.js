1f811e89cd663d1b43ec5c16e19ad2043082c83c
AkiraQZ <vboyrkin@gmail.com>
2023-06-20 04:14:14 +0800

    feat: wt complete

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+const { log } = console;
+
+function WorkRes(status, value) {
+    this.status = status;
+    this.data = value;
+}
+
+function createWorker(counter) {
+    return new Promise((resolve) => {
+        const worker = new Worker('./src/wt/worker.js', { workerData: counter });
+
+        worker.on('message', (value) => resolve(new WorkRes('resolved', value)));
+
+        worker.on('error', () => resolve(new WorkRes('error', null)));
+    });
+}
+
+function* workerGenerator(cpuCount) {
+    let startCPUCounter = 10 + i;
+    for (let index = 0; index < cpuCount; index++) {
+        yield createWorker(startCPUCounter);
+        startCPUCounter += 1;
+    }
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpusCount = os.cpus().length;
+    const promiseArr = [];
+    for (let i = 0; i < cpusCount; i++) {
+        promiseArr.push(createWorker(9 + i));
+    }
+    const result = await Promise.allSettled(promiseAr);
+    const resultArr = result.map((answer) => answer.value);
+    log(resultArr);
 };
 
 await performCalculations();
